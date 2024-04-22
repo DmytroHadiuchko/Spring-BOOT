e62bae3691a47d251892b32d2e9dde02e48d0631
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-04-22 19:29:54 +0300

    changed repository, added liquidbase, added softe delete

--- src/main/java/dmytro/hadiuchko/springboot/controller/BookController.java
+++ src/main/java/dmytro/hadiuchko/springboot/controller/BookController.java
@@ -5,11 +5,15 @@ import dmytro.hadiuchko.springboot.dto.response.BookDto;
 import dmytro.hadiuchko.springboot.service.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -31,5 +35,17 @@ public class BookController {
     @PostMapping
     public BookDto createBook(@RequestBody CreateBookRequestDto bookDto) {
         return bookService.save(bookDto);
+    }
+
+    @PutMapping("/{id}")
+    public void updateById(@PathVariable Long id,
+                           @RequestBody CreateBookRequestDto bookRequestDto) {
+        bookService.updateById(id, bookRequestDto);
+    }
+
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void deleteBookById(@PathVariable Long id) {
+        bookService.deleteById(id);
     }
 }
