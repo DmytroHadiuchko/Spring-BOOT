704909d43e3f85f71f0341cf0885a4d6e6a46d71
DmytroHadiuchko <153805566+DmytroHadiuchko@users.noreply.github.com>
2024-04-29 12:53:53 +0300

    added exception handler, added annotations to dto's clasess (#5)
    * added exception handler, added annotations to dto's clasess
    
    * added message to all annotations
    
    ---------
    
    Co-authored-by: Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>

--- src/main/java/dmytro/hadiuchko/springboot/dto/request/BookSearchParametersDto.java
+++ src/main/java/dmytro/hadiuchko/springboot/dto/request/BookSearchParametersDto.java
@@ -1,4 +1,13 @@
 package dmytro.hadiuchko.springboot.dto.request;
 
-public record BookSearchParametersDto(String title, String author, String isbn) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record BookSearchParametersDto(
+        @NotBlank(message = "title may not be blank")
+        String title,
+        @NotBlank(message = "author may not be blank")
+        String author,
+        @Size(min = 13, message = "ISBN must be at least 13-digit numbers long")
+        String isbn) {
 }
--- src/main/java/dmytro/hadiuchko/springboot/dto/request/CreateBookRequestDto.java
+++ src/main/java/dmytro/hadiuchko/springboot/dto/request/CreateBookRequestDto.java
@@ -1,13 +1,23 @@
 package dmytro.hadiuchko.springboot.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
+    @NotBlank(message = "title may not be blank")
     private String title;
+    @NotBlank(message = "Author may not be blank")
     private String author;
+    @NotBlank
+    @Size(min = 13, message = "ISBN must be at least 13-digit numbers long")
     private String isbn;
+    @NotNull(message = "price may not be null")
+    @Min(value = 0, message = "min value of price is 0")
     private BigDecimal price;
     private String description;
     private String coverImage;
