7807fcd55daee9098bdcc7b6e1aa6eb17f043582
DmytroHadiuchko <153805566+DmytroHadiuchko@users.noreply.github.com>
2024-05-02 12:24:33 +0300

    addet pagination,Sorting, Swagger (#6)
    * addet pagination,Sorting, Swagger
    
    * fixed all comments
    
    * fixed checkstyle
    
    * made messages prettier
    
    ---------
    
    Co-authored-by: Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>

--- src/main/java/dmytro/hadiuchko/springboot/service/BookService.java
+++ src/main/java/dmytro/hadiuchko/springboot/service/BookService.java
@@ -4,11 +4,12 @@ import dmytro.hadiuchko.springboot.dto.request.BookSearchParametersDto;
 import dmytro.hadiuchko.springboot.dto.request.CreateBookRequestDto;
 import dmytro.hadiuchko.springboot.dto.response.BookDto;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto requestDto);
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     BookDto findById(Long id);
 
@@ -16,5 +17,5 @@ public interface BookService {
 
     void updateById(Long id, CreateBookRequestDto bookRequestDto);
 
-    List<BookDto> search(BookSearchParametersDto searchParameters);
+    List<BookDto> search(BookSearchParametersDto searchParameters, Pageable pageable);
 }
