16c039b0d5fd8364ea6c177c79f22d49138df703
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-05-10 18:15:37 +0300

    added empty line

--- src/main/java/dmytro/hadiuchko/springboot/config/SecurityConfig.java
+++ src/main/java/dmytro/hadiuchko/springboot/config/SecurityConfig.java
@@ -21,6 +21,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+
     private final UserDetailsService userDetailsService;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
