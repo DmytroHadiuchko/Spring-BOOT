7375f6d5bf4e9a014f9d9603efe46805dc0d2631
DmytroHadiuchko <153805566+DmytroHadiuchko@users.noreply.github.com>
2024-05-14 14:45:42 +0300

    added Jwt (#9)
    * added roles, access by role
    
    * added jwt
    
    * added roles, access by role, role entity (#8)
    
    * added roles, access by role
    
    * fixed comments
    
    ---------
    
    Co-authored-by: Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
    
    * changed @Data to @Getter and @Setter
    
    * dleted import
    
    * deleted empty line
    
    * added empty line
    
    * added jwt
    
    * changed @Data to @Getter and @Setter
    
    * deleted empty line
    
    * added empty line
    
    * fixed comments
    
    ---------
    
    Co-authored-by: Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>

--- src/main/java/dmytro/hadiuchko/springboot/controller/AuthenticationController.java
+++ src/main/java/dmytro/hadiuchko/springboot/controller/AuthenticationController.java
@@ -1,8 +1,11 @@
 package dmytro.hadiuchko.springboot.controller;
 
+import dmytro.hadiuchko.springboot.dto.user.request.UserLoginRequestDto;
 import dmytro.hadiuchko.springboot.dto.user.request.UserRegistrationRequestDto;
+import dmytro.hadiuchko.springboot.dto.user.response.UserLoginResponseDto;
 import dmytro.hadiuchko.springboot.dto.user.response.UserResponseDto;
 import dmytro.hadiuchko.springboot.exception.RegistrationException;
+import dmytro.hadiuchko.springboot.security.AuthenticationService;
 import dmytro.hadiuchko.springboot.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -19,11 +22,17 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Authentication", description = "Endpoints for user authentication")
 public class AuthenticationController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
     @PostMapping("/register")
     @Operation(summary = "Register new user", description = "Register new user")
     public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto requestDto)
             throws RegistrationException {
         return userService.register(requestDto);
+    }
+
+    @PostMapping("/login")
+    public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto requestDto) {
+        return authenticationService.authenticate(requestDto);
     }
 }
