a215ac205118b49fdef14a985174680deabd75c9
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-05-21 08:26:12 +0300

    added description to controller

--- src/main/java/dmytro/hadiuchko/springboot/controller/OrderController.java
+++ src/main/java/dmytro/hadiuchko/springboot/controller/OrderController.java
@@ -6,6 +6,8 @@ import dmytro.hadiuchko.springboot.dto.order.UpdateOrderStatusRequestDto;
 import dmytro.hadiuchko.springboot.dto.orderitem.OrderItemResponseDto;
 import dmytro.hadiuchko.springboot.entity.User;
 import dmytro.hadiuchko.springboot.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -19,12 +21,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Order management", description = "Endpoints for managing orders")
 @RestController
 @RequestMapping("/api/orders")
 @RequiredArgsConstructor
 public class OrderController {
     private final OrderService orderService;
 
+    @Operation(summary = "Create new order", description = "Create new order")
     @PostMapping
     @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     public OrderResponseDto createOrder(@Valid @RequestBody CreateOrderRequestDto requestDto,
@@ -32,6 +36,7 @@ public class OrderController {
         return orderService.createOrder(requestDto, user.getId());
     }
 
+    @Operation(summary = "Update status", description = "Looking for order by id and update status")
     @PutMapping("/{orderId}")
     @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     public OrderResponseDto updateStatus(@Valid @RequestBody
@@ -41,22 +46,29 @@ public class OrderController {
         return orderService.updateOrder(user.getId(), orderId, requestDto);
     }
 
+    @Operation(summary = "Get all orders",
+            description = "Returns list with user's orders")
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     @GetMapping
     public List<OrderResponseDto> getAllOrders(@AuthenticationPrincipal User user) {
         return orderService.getAllOrdersForUserById(user.getId());
     }
 
+    @Operation(summary = "Get all orders by id",
+            description = "Return list with user's orders by id")
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     @GetMapping("/{orderId}/items")
     public List<OrderItemResponseDto> getAllOrdersById(@PathVariable Long orderId,
                                                  @AuthenticationPrincipal User user) {
         return orderService.getOrderItemsByOrdersId(orderId, user.getId());
     }
 
+    @Operation(summary = "Get order item", description = "Returns order item")
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     @GetMapping("/{orderId}/items/{itemId}")
     public OrderItemResponseDto getOrderItem(@PathVariable Long orderId,
                                              @PathVariable Long itemId,
                                              @AuthenticationPrincipal User user) {
         return orderService.getOrderItemById(orderId, user.getId(), itemId);
     }
-
 }
