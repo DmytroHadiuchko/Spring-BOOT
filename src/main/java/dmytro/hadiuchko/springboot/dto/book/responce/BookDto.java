d07225bd90db7de19b554dfd309aa2ee00834cc5
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-05-28 15:45:35 +0300

    added changes to mapper

--- src/main/java/dmytro/hadiuchko/springboot/dto/book/responce/BookDto.java
+++ src/main/java/dmytro/hadiuchko/springboot/dto/book/responce/BookDto.java
@@ -1,6 +1,7 @@
 package dmytro.hadiuchko.springboot.dto.book.responce;
 
 import java.math.BigDecimal;
+import java.util.Set;
 import lombok.Data;
 
 @Data
@@ -12,4 +13,5 @@ public class BookDto {
     private BigDecimal price;
     private String description;
     private String coverImage;
+    Set<Long> categoryIds;
 }
--- src/main/java/dmytro/hadiuchko/springboot/mapper/CategoryMapper.java
+++ src/main/java/dmytro/hadiuchko/springboot/mapper/CategoryMapper.java
@@ -1,14 +1,24 @@
 package dmytro.hadiuchko.springboot.mapper;
 
+import java.util.Set;
+import java.util.stream.Collectors;
 import dmytro.hadiuchko.springboot.config.MapperConfig;
 import dmytro.hadiuchko.springboot.dto.category.request.CategoryRequestDto;
 import dmytro.hadiuchko.springboot.dto.category.responce.CategoryResponseDto;
 import dmytro.hadiuchko.springboot.entity.Category;
 import org.mapstruct.Mapper;
+import org.mapstruct.Named;
 
 @Mapper(config = MapperConfig.class)
 public interface CategoryMapper {
     CategoryResponseDto toDto(Category category);
 
     Category toModel(CategoryRequestDto dto);
+
+    @Named("categoriesById")
+    default Set<Category> categoriesByIds(Set<Long> categories) {
+        return categories.stream()
+                .map(Category::new)
+                .collect(Collectors.toSet());
+    }
 }
